82ee78ae07f984db1b28aba51c12b2a5f306f9e1
joelpinheiro <joelpinheiro@ua.pt>
2016-04-04 15:26:18 +0100

    .

--- itrading/js/scripts.js
+++ itrading/js/scripts.js
@@ -117,7 +117,7 @@
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+          $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later or send the message directly to joelpinh@gmail.com!");
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
